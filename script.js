c21c40a26e3f98aa274ae6abdf33b039861227e8
totsky.ia <valihmanov777@gmail.com>
2023-11-01 12:23:43 +0300

    Добавлено задание 26го урока

---
+++ script.js
@@ -0,0 +1,38 @@
+// Функция getData для загрузки данных из файла db.json
+function getData() {
+  return fetch("db.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Ошибка при получении данных");
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// Функция sendData для отправки данных из файла db.json
+function sendData(data) {
+  return fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Ошибка при отправке данных");
+      } else {
+        console.log("Данные успешно отправлены");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+getData().then((data) => {
+  sendData(data);
+});
